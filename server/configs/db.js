1494ec4184d1409620e2148aa17d4128dac1ed89
FruitWala <skbusiness795@gmail.com>
2026-02-05 19:28:38 +0530

    Fix Address model casing and userId ref

--- server/configs/db.js
+++ server/configs/db.js
@@ -1,12 +1,16 @@
 import mongoose from "mongoose";
 
-const connectDB = async ()=>{
-    try {
-        mongoose.connection.on('connected', () => console.log("Database Connected"));
-        await mongoose.connect(`${process.env.MONGODB_URI}/greencart`)
-    } catch (error) {
-        console.error(error.message);
-    }
-}
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+
+    mongoose.connection.on("connected", () => {
+      console.log("Database Connected");
+    });
+  } catch (error) {
+    console.error("Database connection error:", error.message);
+    process.exit(1);
+  }
+};
 
 export default connectDB;
